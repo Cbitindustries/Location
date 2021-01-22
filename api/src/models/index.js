51d7f535004c3491ed3dfc4cd0910a0fd4d4be7e
Cbitindustries <emmanuel@cbitindustries.com>
2021-01-22 11:07:40 +0100

    fixed axios vulnerabilities

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -24,7 +24,8 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    const model = sequelize["import"](path.join(__dirname, file));
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    // const model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 Object.keys(db).forEach((modelName) => {
